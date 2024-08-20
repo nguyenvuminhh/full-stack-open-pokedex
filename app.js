ac401bb1c7e9871825b8f63d0054f455262b7852
LAPTOP <nvminh19051905@gmail.com>
2024-08-20 12:41:39 +0700

    test health check 2

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/api/health', (_req, res) => {
+  // eslint-disable-next-line no-constant-condition
   if (true) res.status(400)
   res.send('ok')
 })
